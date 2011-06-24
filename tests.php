316cabec105a69c220c36c96d98a76fd307da8c7
Joakim Nystrom <jnystromdesign@gmail.com>
2011-06-24 12:05:13 +0200

    Limited the request and saves the tokens in a Cookie. Creted an ugly test request to the latest added items. Also created a function for curling. Still need to cleanup this mess, but hey; it is actually working.

--- tests.php
+++ tests.php
@@ -10,24 +10,8 @@
 	$userPassword = USER_PASSWORD;
 	
 	
-	
-	// Set up signature key for signing stuff...
-	
-	//  BUG - Can't resolve a response.... :( Seems like I'm capped. :o
-	function authentication()
-	{
-		global $myAppToken, $myAppId;
+	function curlThis($url){
 		
-		$userid = $_COOKIE['userid'];
-		$key = md5($userid.$myAppToken);
-		$url = 'http://api.toodledo.com/2/account/token.php?userid=' . $userid . ';appid=' . $myAppId . ';sig=' . $key;
-		
-		print $key .'<br>';
-		print $userid .'<br>';
-		print $url . '<br>';
-		
-		
-		/*
 		$ch = curl_init();
 
 		curl_setopt($ch, CURLOPT_URL, $url);
@@ -37,27 +21,19 @@
 		$output = curl_exec($ch);
 
 		curl_close($ch);
-
-		$object = json_decode($output);
 		
-		print '<pre>';
-		print_r($object);
-		print '</pre>';
-		*/
+		$object = json_decode($output, TRUE);
+				
+		return $object; 
 	}
 	
-	
-	authentication();
-	
-	function setUserID()
+	// Set up signature key for signing stuff...
+	function setUserID($myAppId, $userEmail, $userPassword)
 	{
 		/*--- Account Lookup ---*/
 		global $userEmail, $myAppToken;
 		
 		$sig = md5($userEmail.$myAppToken );
-		print '<pre>';
-		print_r($sig);
-		print '</pre>';
 		$ch = curl_init();
 		$url = "http://api.toodledo.com/2/account/lookup.php?appid=$myAppId;sig=$sig;email=$userEmail;pass=$userPassword";
 
@@ -75,6 +51,33 @@
 
 	}
 	
+	function createSessionToken()
+	{
+		global $myAppToken, $myAppId;
+		$userid = $_COOKIE['userid'];
+		$key = md5($userid.$myAppToken);
+		$url = 'http://api.toodledo.com/2/account/token.php?userid=' . $userid . ';appid=' . $myAppId . ';sig=' . $key;
+		
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+
+		$output = curl_exec($ch);
+
+		curl_close($ch);
+
+		$object = json_decode($output);
+		
+		return $object;
+	}
+	
+	function createKey($userPassword, $appToken, $sessionToken){
+		$key = md5( md5($userPassword).$appToken.$sessionToken);
+		return $key;
+	}
+	
 	function unsetUserId()
 	{
 		unset($_COOKIE['userid']);
@@ -84,14 +87,37 @@
 	
 	// Run only if cookie isn't set when user haven't asked to log out.
 	if(!isset($_COOKIE['userid']) && !isset($_GET['logout'])){
-		setUserID();
+		setUserID($myAppId, $userEmail, $userPassword);
 	}
+	
+	// Run if the user Id is set and if no authtoken is set. (We'll get capped if we require more than 10 token per hours...)
+	if(isset($_COOKIE['userid'])){
+		if(!isset($_COOKIE['sessionToken'])){
+			$authObject = createSessionToken();
+			setCookie('sessionToken', $authObject->token);
+		}
+	}
+	
 	
 	// Run if user choosed to log out.
 	if(isset($_GET['logout'])){
 		unsetUserId();
 	}
+	/*
+	$apiKey = 'c3c094da7058293e58aa94e67ab4c661';
+	$method = 'flickr.photos.search';
+	$text = 'cat';
+	$url= "http://api.flickr.com/services/rest/?method=$method&api_key=$apiKey&text=$text&format=json&nojsoncallback=1&per_page=20";
+	*/ 
+	$sessionToken = $_COOKIE['sessionToken'];
+	$key = createKey($userPassword, $myAppToken, $sessionToken);
+	$url = "http://api.toodledo.com/2/tasks/get.php?key=$key;modafter=1234567890;fields=folder,star,priority";
+	$object = curlThis($url);
 	
+	print $url;
+	print '<pre>';
+	print_r($object);
+	print '</pre>';
 
 
 	
